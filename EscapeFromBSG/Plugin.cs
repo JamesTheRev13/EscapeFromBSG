691eeebb48a9e6e376772e76d026490bb4a84a9d
JamesTheRev13 <47574329+JamesTheRev13@users.noreply.github.com>
2024-05-02 20:22:24 -0600

    Add project files.

---
+++ EscapeFromBSG/Plugin.cs
@@ -0,0 +1,13 @@
+﻿// PLUGIN BY BOB SAGET
+// FUCK BATTLESTATE GAMES
+
+using BepInEx;
+
+namespace EscapeFromBSG
+{
+    [BepInPlugin("BobSaget.EscapeFromBSG", "EscapeFromBSG", "1.0.0")]
+    public class Plugin : BaseUnityPlugin
+    {
+        
+    }
+}
